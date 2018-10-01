7ea7338d1f251bafab0adf13d409ab9a6dbedaf1
onionpower <chocoonion@gmail.com>
2018-10-02 01:39:34 +0300

    1.3: mutate string, fix an annoying mistake with an allocated string

--- src/1.3/main.cpp
+++ src/1.3/main.cpp
@@ -10,22 +10,46 @@ string Urlify(string s) {
     }
   }
   char url[s.length() + 2 * spaces];
-  for (int i = 0; i < s.length(); i++){
+  for (int i = 0, j = 0; i < s.length(); i++, j++){
     if (s[i] == ' '){
-      url[i] = '%';
-      url[i+1] = '2';
-      url[i+2] = '0';
+      url[j] = '%';
+      url[j+1] = '2';
+      url[j+2] = '0';
+      j+=2;
       continue;
     }
-    url[i] = s[i];
+    url[j] = s[i];
   }  
   return url;
 }
 
+void UrlifyM(string& s) {
+  int spaces = 0, size = s.length();
+  for (int i = 0; i < size; i++){
+   if (s[i] == ' '){
+      spaces++;
+    }
+  }  
+  int newSize = s.length() + spaces * 2;
+  s.resize(newSize);
+  for (int i = size - 1, j = newSize - 1; i >= 0; i--, j--){
+    if (s[i] == ' '){
+      s[j] = '0';
+      s[j-1] = '2';
+      s[j-2] = '%';
+      j -= 2;
+      continue;
+    }
+    s[j] = s[i];
+  }
+}
+
 int main() {
-  auto s = "a string with spaces";
+  string s = "a string with spaces";
   cout << s << endl;
   auto url = Urlify(s);
   cout << url << endl;
+  UrlifyM(s);
+  cout << s << endl;
   return 0;
 }
