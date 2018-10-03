699993f54cdb7919a986d25a7244133cdafe7187
onionpower <chocoonion@gmail.com>
2018-10-04 01:53:29 +0300

    1.4: a space efficient way

--- src/1.4/main.cpp
+++ src/1.4/main.cpp
@@ -3,12 +3,38 @@
 
 using namespace std;
 
+int getShift(char c){
+  return c - 'a';
+}
+
+bool isPalPermSpaceEfficient(string s) {  
+  int check = 0;
+  for (int i = 0; i < s.size(); i++){
+    auto c = s[i];
+    if (c == ' '){
+      continue;
+    }
+    if (c > 'z' || c < 'a'){
+      return false;
+    }
+    int shift = getShift(s[i]);
+    check = check ^ 1 << shift;
+  }
+  if (check == 0) {
+    return true;
+  }
+  return (check & (check - 1)) == 0;
+}
+
 bool isPalPerm(string s){
   unordered_map<char, int> map;
   for (int i = 0; i < s.size(); i++){
     auto c = s[i];
     if (c == ' '){
       continue;
+    }
+    if (c > 'z' || c < 'a'){
+      return false;
     }
     auto occ = map[c];
     map[c] = occ + 1;    
@@ -31,9 +57,15 @@ bool isPalPerm(string s){
 }
 
 int main() {
-  auto palPerm = "hemo moeh";
-  auto notPalPerm = "hemo moah";
-  cout << palPerm << " is palindrome permutation: " << isPalPerm(palPerm) << endl;
-  cout << notPalPerm << " is palindrome permutation: " << isPalPerm(notPalPerm) << endl;
+  string test[] = {"hemo moeh", "hemo moah", "aaa11 1", "bbaaccd", "bba abbssdde e", "b baed d", "mmmaaa", "aa11b"};
+  cout << "isPalPerm" << endl;
+  for (auto t : test){
+    cout << t << " is palindrome permutation: " << isPalPerm(t) << endl;
+  }
+
+  cout << "isPalPermSpaceEfficient" << endl;
+  for (auto t : test){
+    cout << t << " is palindrome permutation: " << isPalPermSpaceEfficient(t) << endl;
+  }  
   return 0;
 }
