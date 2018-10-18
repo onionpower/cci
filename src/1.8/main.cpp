5f6f7d93661fee917968b7141d13f8de091f1e45
onionpower <chocoonion@gmail.com>
2018-10-18 11:19:04 +0300

    1.8: sloppy

--- src/1.8/main.cpp
+++ src/1.8/main.cpp
@@ -20,13 +20,88 @@ string toString(int *m, int r, int c){
   return res;
 }
 
+void memInFirstRowsAndCols(int *m, int r, int c) {
+  for (int i = 1; i < r; i++) {
+    for (int j = 1; j < c; j++) {
+      if (m[i*c+j] == 0) {
+        m[i*c] = 0;
+        m[j] = 0;
+      }
+    }
+  }
+}
+
+void nullifyRows(int *m, int r, int c) {
+  for (int i = 0; i < r; i++) {
+    if (m[i*c] == 0) {
+      for (int j = 1; j < c; j++) {
+        m[i*c+j] = 0;
+      }
+    }
+  }
+}
+
+void nullifyCols(int *m, int r, int c) {
+  for (int i = 0; i < c; i++) {
+    if (m[i] == 0) {
+      for (int j = 1; j < r; j++) {
+        m[j*c+i] = 0;
+      }
+    }
+  }
+}
+
+void nullifyFirstRow(int *m, int c) {
+  for (int i = 0; i < c; i++) {
+    m[i] = 0;
+  }
+}
+
+void nullifyFirstCol(int *m, int r, int c) {
+  for (int i = 0; i < r; i++) {
+    m[i*c] = 0;
+  }
+}
+
+void nullify(int *m, int r, int c) {
+  bool firstRowHasZero = false;
+  bool firstColHasZero = false;
+
+  for (int i = 0; i < c; i++) {
+    if (m[i] == 0) {
+      firstRowHasZero = true;
+      break;
+    }
+  }
+
+  for (int i = 0; i < r; i++) {
+    if (m[i*r] == 0) {
+      firstColHasZero = true;
+      break;
+    }
+  }
+
+  memInFirstRowsAndCols(m, r, c);
+  nullifyRows(m, r, c);
+  nullifyCols(m, r, c);
+
+  if (firstRowHasZero) {
+    nullifyFirstRow(m, r);
+  }
+  if (firstColHasZero) {
+    nullifyFirstCol(m, r, c);
+  }
+}
+
 int main() {  
   int m[4][4] = {
-    {1, 2, 0, 3},
+    {1, 2, 1, 3},
     {0, 2, 3, 0},
     {1, 2, 3, 4},
     {1, 2, 3, 0}
   };
+  cout << toString(*m, 4, 4) << endl;
+  nullify(*m, 4, 4);
   cout << toString(*m, 4, 4) << endl;
   return 0;
 }
