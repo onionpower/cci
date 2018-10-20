0b86015a68258fa3915535669775f3d4d5b67f75
onionpower <chocoonion@gmail.com>
2018-10-20 10:18:25 +0300

    1.9

--- src/1.9/main.cpp
+++ src/1.9/main.cpp
@@ -2,6 +2,23 @@
 
 using namespace std;
 
-int main() {    
+void printIsRotated(string s1, string rotated) {
+  string conc = s1 + s1;
+  size_t place = conc.find(rotated);
+  string conclusion;
+  if (place == string::npos) {
+    conclusion = "\" isn't rotated \"";
+  } else {
+    conclusion = "\" is rotated \"";
+  }
+  cout << "\"" << rotated << conclusion << s1 << "\"" << endl;
+}
+
+int main() {
+  string s = "origin string";
+  string rotated = "gin stringori";
+  string noise = "gin strinPori";    
+  printIsRotated(s, rotated);
+  printIsRotated(s, noise);
   return 0;
 }
