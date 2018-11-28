e81cb714679a3af7142ce87438fa521c69db04f8
onionpower <chocoonion@gmail.com>
2018-11-28 18:44:23 +0300

    2.5: mul

--- src/2.5/main.cpp
+++ src/2.5/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 typedef std::forward_list<int>::iterator fwd_iterator;
 typedef std::forward_list<int> fwd_list;
 
-int compose(fwd_list l) {
+int compose_using_queue(fwd_list l) {
   queue<int> s;
   
   for(auto& it : l)
@@ -24,11 +24,38 @@ int compose(fwd_list l) {
   return r;
 }
 
+int compose_using_mul(fwd_list l) {
+  auto r = 0;
+  auto mul = 1;
+  for(auto& it : l)
+  {
+    r += it * mul;
+    mul *= 10;
+  }
+  return r;
+}
+
+int sum_lists_using_queue(fwd_list l1, fwd_list l2) {
+  auto sum = compose_using_queue(l1) + compose_using_queue(l2);
+  return sum;
+}
+
+int sum_lists_using_mul(fwd_list l1, fwd_list l2) {
+  auto sum = compose_using_mul(l1) + compose_using_mul(l2);
+  return sum;
+}
+
 int main() {
   fwd_list l1 = {1, 2, 3};
   fwd_list l2 = {4, 5, 6, 7};
   // 321 + 7654 = 7975
-  auto sum = compose(l1) + compose(l2);
-  cout << "sum = " << sum << endl;
+  auto sum = sum_lists_using_queue(l1, l2);
+  cout << "queue: sum = " << sum << endl;
+  sum = sum_lists_using_mul(l1, l2);
+  cout << "mul: sum = " << sum << endl;
+  int i = 1;
+  cout << i << endl;
+  i = i << 1;
+  cout << i << endl;
   return 0;
 }
